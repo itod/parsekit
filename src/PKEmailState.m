26b48cf5f3192de2ce02a9fb23aa0c190c437e8f
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-04-17 15:46:06 -0500

    * revert porting of delimit state changes from pegkit

--- src/PKEmailState.m
+++ src/PKEmailState.m
@@ -1,30 +1,16 @@
-// The MIT License (MIT)
 //
-// Copyright (c) 2014 Todd Ditchendorf
+//  PKEmailState.m
+//  ParseKit
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//  Created by Todd Ditchendorf on 3/31/10.
+//  Copyright 2010 Todd Ditchendorf. All rights reserved.
 //
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
 
-#import <PEGKit/PKEmailState.h>
-#import <PEGKit/PKReader.h>
-#import <PEGKit/PKTokenizer.h>
-#import <PEGKit/PKToken.h>
-#import <PEGKit/PKTypes.h>
+#import <ParseKit/PKEmailState.h>
+#import <ParseKit/PKReader.h>
+#import <ParseKit/PKTokenizer.h>
+#import <ParseKit/PKToken.h>
+#import <ParseKit/PKTypes.h>
 
 @interface PKToken ()
 @property (nonatomic, readwrite) NSUInteger offset;
@@ -35,14 +21,11 @@
 - (void)append:(PKUniChar)c;
 - (NSString *)bufferedString;
 - (PKTokenizerState *)nextTokenizerStateFor:(PKUniChar)c tokenizer:(PKTokenizer *)t;
-@property (nonatomic) NSUInteger offset;
 @end
 
 @interface PKEmailState ()
 - (BOOL)parseNameFromReader:(PKReader *)r;
 - (BOOL)parseHostFromReader:(PKReader *)r;
-@property (nonatomic) PKUniChar c;
-@property (nonatomic) PKUniChar lastChar;
 @end
 
 @implementation PKEmailState
@@ -53,7 +36,7 @@
 
 
 - (void)append:(PKUniChar)ch {
-    self.lastChar = ch;
+    lastChar = ch;
     [super append:ch];
 }
 
@@ -62,26 +45,26 @@
     NSParameterAssert(r);
     [self resetWithReader:r];
     
-    self.lastChar = PKEOF;
-    self.c = cin;
+    lastChar = PKEOF;
+    c = cin;
     BOOL matched = [self parseNameFromReader:r];
     if (matched) {
         matched = [self parseHostFromReader:r];
     }
 
-    if (PKEOF != _c) {
+    if (PKEOF != c) {
         [r unread];
     }
     
     NSString *s = [self bufferedString];
     if (matched) {
-        if ('.' == _lastChar) {
+        if ('.' == lastChar) {
             s = [s substringToIndex:[s length] - 1];
             [r unread];
         }
         
-        PKToken *tok = [PKToken tokenWithTokenType:PKTokenTypeEmail stringValue:s doubleValue:0.0];
-        tok.offset = self.offset;
+        PKToken *tok = [PKToken tokenWithTokenType:PKTokenTypeEmail stringValue:s floatValue:0.0];
+        tok.offset = offset;
         return tok;
     } else {
         [r unread:[s length] - 1];
@@ -95,17 +78,17 @@
     BOOL hasAtLeastOneChar = NO;
 
     for (;;) {
-        if (PKEOF == _c || isspace(_c)) {
+        if (PKEOF == c || isspace(c)) {
             result = NO;
             break;
-        } else if ('@' == _c && hasAtLeastOneChar) {
+        } else if ('@' == c && hasAtLeastOneChar) {
             //[self append:c];
             result = YES;
             break;
         } else {
             hasAtLeastOneChar = YES;
-            [self append:_c];
-            self.c = [r read];
+            [self append:c];
+            c = [r read];
         }
     }
     
@@ -120,16 +103,16 @@
     
     // ^[:space:]()<>/"'
     for (;;) {
-        if (PKEOF == _c || isspace(_c) || '(' == _c || ')' == _c || '<' == _c || '>' == _c || '/' == _c || '"' == _c || '\'' == _c) {
+        if (PKEOF == c || isspace(c) || '(' == c || ')' == c || '<' == c || '>' == c || '/' == c || '"' == c || '\'' == c) {
             result = hasAtLeastOneChar && hasDot;
             break;
         } else {
-            if ('.' == _c) {
+            if ('.' == c) {
                 hasDot = YES;
             }
             hasAtLeastOneChar = YES;
-            [self append:_c];
-            self.c = [r read];
+            [self append:c];
+            c = [r read];
         }
     }
     
--- src/PKSymbolNode.m
+++ src/PKSymbolNode.m
@@ -1,33 +1,25 @@
-// The MIT License (MIT)
-// 
-// Copyright (c) 2014 Todd Ditchendorf
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy 
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is 
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+//  Copyright 2010 Todd Ditchendorf
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
-#import "PKSymbolNode.h"
-#import "PKSymbolRootNode.h"
+#import <ParseKit/PKSymbolNode.h>
+#import <ParseKit/PKSymbolRootNode.h>
 
 @interface PKSymbolNode ()
-@property (nonatomic, retain, readwrite) NSString *ancestry;
+@property (nonatomic, readwrite, retain) NSString *ancestry;
 @property (nonatomic, assign) PKSymbolNode *parent;  // this must be 'assign' to avoid retain loop leak
 @property (nonatomic, retain) NSMutableDictionary *children;
-@property (nonatomic, assign) PKUniChar character;
+@property (nonatomic) PKUniChar character;
 @property (nonatomic, retain) NSString *string;
 
 - (void)determineAncestry;
@@ -44,7 +36,7 @@
 
         // this private property is an optimization. 
         // cache the NSString for the char to prevent it being constantly recreated in -determineAncestry
-        self.string = [NSString stringWithFormat:@"%C", (unichar)_character];
+        self.string = [NSString stringWithFormat:@"%C", (unichar)character];
 
         [self determineAncestry];
     }
@@ -53,7 +45,7 @@
 
 
 - (void)dealloc {
-    self.parent = nil;
+    parent = nil; // makes clang static analyzer happy
     self.ancestry = nil;
     self.string = nil;
     self.children = nil;
@@ -62,8 +54,8 @@
 
 
 - (void)determineAncestry {
-    if (PKEOF == _parent.character) { // optimization for sinlge-char symbol (parent is symbol root node)
-        self.ancestry = _string;
+    if (PKEOF == parent.character) { // optimization for sinlge-char symbol (parent is symbol root node)
+        self.ancestry = string;
     } else {
         NSMutableString *result = [NSMutableString string];
         
@@ -83,4 +75,9 @@
     return [NSString stringWithFormat:@"<PKSymbolNode %@>", self.ancestry];
 }
 
+@synthesize ancestry;
+@synthesize parent;
+@synthesize character;
+@synthesize string;
+@synthesize children;
 @end
